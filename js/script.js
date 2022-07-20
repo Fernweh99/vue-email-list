6b817d6ca01a2644bcabceb11770eaa0f2bfb8c9
Fernweh99 <daniele.dimento@gmail.com>
2022-07-20 15:49:10 +0200

    finish ex

--- js/script.js
+++ js/script.js
@@ -3,5 +3,16 @@ const app = new Vue({
   name: 'app',
   el: "#root",
   data: {
+    linkRandomMail: "https://flynn.boolean.careers/exercises/api/random/mail",
+    numbersOfMail: 10,
+    mails: [],
   },
+  created(){
+    for (let i = 0; i < 10; i++) {
+      axios.get(this.linkRandomMail)
+      .then(res=>{
+        this.mails.push(res.data.response);
+      })
+    }
+  }
 })
